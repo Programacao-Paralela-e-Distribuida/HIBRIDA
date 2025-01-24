b9852b8b2a7434bb22aa20e739d101e8e52ef46d
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-01-23 22:22:14 -0300

    Primeiras Modificações

--- old/mpi_openmp_offloading.c
+++ old/mpi_openmp_offloading.c
@@ -5,7 +5,7 @@ static long num_steps = 100000000;
 double step;
 int main(int argc, char *argv[])
 {
-  int i, rank, size, provided;
+  int i, ranque, numprocs, provided;
   double x, pi, sum = 0.0, global_sum = 0.0;
   double start_time, run_time;
   // Inicia o MPI com suporte para threads
@@ -14,19 +14,19 @@ int main(int argc, char *argv[])
     printf("Nível de suporte para threads não é suficiente!\n");
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // O rank do processo
-  MPI_Comm_size(MPI_COMM_WORLD, &size); // O número de processos
+  MPI_Comm_rank(MPI_COMM_WORLD, &ranque); // O rank do processo
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs); // O número de processos
   step = 1.0 / (double)num_steps;
   start_time = omp_get_wtime(); // Tempo de início da execução
 // Offloading com OpenMP para o acelerador (GPU), com paralelismo dentro do processo
-#pragma omp target teams distribute parallel for default(none) firstprivate(num_steps, step) reduction(+ : sum) map(tofrom : sum) private(x) device(0)
-  for (i = rank; i < num_steps; i += size) { // Saltos de acordo com o número de processos
+#pragma omp target teams distribute parallel for default(none) firstprivate(num_steps, step, ranque, numprocs) reduction(+ : sum) map(tofrom : sum) private(x) device(0)
+  for (i = ranque; i < num_steps; i += numprocs) { // Saltos de acordo com o número de processos
     x = (i + 0.5) * step;
     sum += 4.0 / (1.0 + x * x);
   }
   // MPI_Reduce para somar os resultados de todos os processos MPI
   MPI_Reduce(&sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (rank == 0) {
+  if (ranque == 0) {
     pi = step * global_sum; // Só o processo 0 calcula o valor final de Pi
     run_time = omp_get_wtime() - start_time;
     printf("pi = %lf, %ld passos, computados em %lf segundos\n", pi, num_steps, run_time);

62ad724281b93f38f514d7cc09a0428bf6384e1c
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-07-12 15:53:38 -0300

    Update mpi_omp_off_calcpi.c

--- src/mpi_omp_off_calcpi.c
+++ src/mpi_omp_off_calcpi.c
@@ -1,60 +1,61 @@
 #include <stdio.h>
-#include "mpi.h"
+#include <mpi.h>
 #include <omp.h>
-#include <stdlib.h>
-#include <time.h>
-// Defina a proporção da carga de trabalho para a GPU (em %)
-#define GPU_WORKLOAD 70
-static long int num_passos = 1000000000 ; // Número total de pontos aleatórios
-int main(int argc, char *argv[]) {
-    long int gpu_contagem_local = 0, cpu_contagem_local = 0, contagem_global = 0;
-    int ranque, numprocs, provided;
-    double x, y, z, pi;
-    double inicio, tempo;
-    // Inicializa o MPI com suporte para threads
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
-    if (provided < MPI_THREAD_FUNNELED) {
-        printf("Nível de suporte para threads não é suficiente!\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-    MPI_Comm_rank(MPI_COMM_WORLD, &ranque); // O rank do processo
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs); // O número de processos
-    // Inicializa o gerador de números aleatórios
-    srand(time(NULL) + ranque);
-    // Define a carga de trabalho para a GPU e para as threads CPU
-    long int passos_gpu = num_passos * GPU_WORKLOAD / 100;
-    long int passos_cpu = num_passos - passos_gpu;
-    inicio = omp_get_wtime(); // Tempo de início da execução
-    // Offloading com OpenMP para a GPU - Master thread
-    #pragma omp parallel num_threads(5)
-    {
-        // Thread master para comunicação e offloading para a GPU
-        #pragma omp master
-        #pragma omp target data map(tofrom:gpu_contagem_local) map(alloc:x,y,z) device(1)
-        #pragma omp target teams distribute parallel for reduction(+:gpu_contagem_local) nowait 
-            for (long int i = ranque; i < passos_gpu; i += numprocs) {
-                double x = (double)rand() / RAND_MAX;
-                double y = (double)rand() / RAND_MAX;
-                double z = x * x + y * y;
-                if (z <= 1.0) gpu_contagem_local++; // Verifica se o ponto está no círculo
-            }
-            // Threads secundárias para execução no CPU
-        #pragma omp for reduction(+:cpu_contagem_local)
-            for (long int i = ranque; i < passos_cpu; i += numprocs) {
-                double x = (double)rand() / RAND_MAX;
-                double y = (double)rand() / RAND_MAX;
-                double z = x * x + y * y;
-                if (z <= 1.0) cpu_contagem_local++;
-            }
-    }
-    // MPI_Reduce para somar os resultados de todos os processos MPI
-    long int local_count_total = gpu_contagem_local + cpu_contagem_local;
-    MPI_Reduce(&local_count_total, &contagem_global, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (ranque == 0) {
-        pi = ((double)contagem_global / (double)num_passos) * 4.0; // Estimação de Pi
-        tempo = omp_get_wtime() - inicio;
-        printf("pi = %3.15f, %ld amostras, computados em %lf segundos\n", pi, num_passos, tempo);
-    }
-    MPI_Finalize(); // Finaliza o MPI
-    return 0;
+const long num_passos = 10000000000;
+
+int main(int argc, char *argv[]) { /* mpi_omp_off_calcpi.c */
+  int meu_ranque, num_procs, fornecido;
+  double soma_local = 0.0, soma_global = 0.0;
+  double inicio, fim, soma_cpu = 0.0, soma_gpu = 0.0;
+  double passo = 1.0 / (double)num_passos; 
+  
+  // Inicia o MPI com suporte para threads
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &fornecido);
+  if (fornecido < MPI_THREAD_FUNNELED) {
+      printf("Nível de suporte para threads não é suficiente!\n");
+      MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque); 
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs); 
+  
+  // Divisão da carga entre GPU e CPUs
+  long int split_point = (int)(num_passos * .7); // 30% para CPU, 70% para GPU
+
+  // Início da execução
+  inicio = omp_get_wtime(); 
+  // Número de threads igual número de processadores + 1
+  #pragma omp parallel num_threads(9)
+  {
+      // Thread master para comunicação e offloading para a GPU
+      #pragma omp master 
+      #pragma omp target data map(tofrom:soma_gpu) map(to:split_point) device(1)
+      #pragma omp target teams distribute parallel for nowait reduction(+:soma_g
+pu)
+      // Saltos de acordo com o número de processos (MPI)
+      for (long int i = meu_ranque; i <= split_point; i += num_procs) { 
+           double x = (i + 0.5) * passo;
+           soma_gpu += 4.0 / (1.0 + x * x);
+      }
+      // Threads secundárias para execução na CPU
+      #pragma omp for reduction(+:soma_cpu)
+      for (long int i = split_point+(meu_ranque+1); i < num_passos; i += num_pro
+cs) { 
+           double x = (i + 0.5) * passo;
+           soma_cpu += 4.0 / (1.0 + x * x);
+      }
+  }
+  // Calcula o valor local do processo
+  soma_local = soma_gpu + soma_cpu;
+  // MPI_Reduce soma os resultados parciais dos processos MPI
+  MPI_Reduce(&soma_local, &soma_global, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORL
+D);
+  double pi = passo * soma_global; 
+  // Processo raiz (0) finaliza e imprime os resultadr]os
+  if (meu_ranque == 0) {     
+      fim = omp_get_wtime();
+      printf("Valor calculado de Pi: %3.15f em %lf segundos\n", pi, fim-inicio);
+  } 
+  // Finaliza o MPI
+  MPI_Finalize();
+  return 0;
 }

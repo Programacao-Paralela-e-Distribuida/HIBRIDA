30c0e18200d8e4e2683db8fee4eb5a0cc52a183b
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-07-12 15:56:25 -0300

    Update mpi_calcpi.c

--- src/mpi_calcpi.c
+++ src/mpi_calcpi.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include "mpi.h"
-static long num_passos = 10000000000;
-double passo;
-int main(int argc, char *argv[]) {
-  int ranque, numprocs, provided;
-  double x, pi, soma = 0.0, soma_global = 0.0;
-  double inicio, tempo;
-  // Inicia o MPI com suporte para threads
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
-  if (provided < MPI_THREAD_FUNNELED) {
-     printf("Nível de suporte para threads não é suficiente!\n");
-     MPI_Abort(MPI_COMM_WORLD, 1);
-  }
-  MPI_Comm_rank(MPI_COMM_WORLD, &ranque); // O rank do processo
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs); // O número de processos
-  passo = 1.0 / (double)num_passos;
+#include <mpi.h>
+const long num_passos = 10000000000;
+int main(int argc, char *argv[]) {  /* mpi_calcpi.c */
+  int meu_ranque, num_procs;
+  double soma = 0.0, soma_global = 0.0;
+  double inicio, fim;
+  double passo = 1.0 / (double) num_passos;
+  MPI_Init(&argc, &argv); // Inicia o MPI 
+  MPI_Comm_rank(MPI_COMM_WORLD, &meu_ranque);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
   inicio = MPI_Wtime(); // Tempo de início da execução
-  for (long int i = ranque; i < num_passos; i += numprocs) { // Saltos de acordo com o número de processos
-     x = (i + 0.5) * passo;
-     soma += 4.0 / (1.0 + x * x);
+  // Saltos de acordo com o número de processos
+  for (long int i = meu_ranque; i < num_passos; i += num_procs) {
+       double x = (i + 0.5) * passo;
+       soma += 4.0 / (1.0 + x * x);
   }
-  // MPI_Reduce para somar os resultados de todos os processos MPI
+  // MPI_Reduce soma os resultados parciais dos processos MPI
   MPI_Reduce(&soma, &soma_global, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  if (ranque == 0) {
-     pi = passo * soma_global; // Só o processo 0 calcula o valor final de Pi
-     tempo = MPI_Wtime() - inicio;
-     printf("pi = %2.15f, %ld passos, computados em %lf segundos\n", pi, num_passos, tempo);
-  }  
+  double pi = passo * soma_global;
+  if (meu_ranque == 0) { // Processo 0 calcula o valor final de Pi
+      fim = MPI_Wtime();
+      printf("Valor calcula de Pi: %2.15f em %lf segundos\n", pi, fim-inicio);
+  }
   MPI_Finalize(); // Finaliza o MPI
   return 0;
 }

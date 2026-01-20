ff428ead0f178753d53f4df07414bc8138b8a5e5
Gabriel P. Silva <gabriel.silva@gmail.com>
2026-01-20 17:41:32 -0300

    Adicionando exemplo primos com OpenMP offloading

--- src/off_primos.c
+++ src/off_primos.c
@@ -1,64 +1,61 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 #include <omp.h>
-// Função para verificar se um número é primo
-bool is_prime(long int n) {
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (long int i = 5; i * i <= n; i += 6) 
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    return true;
+
+int is_prime(long int n) {
+    if (n <= 1) return 0;
+    if (n <= 3) return 1;
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+    for (long int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return 0;
+    }
+    return 1;
 }
+#define GPU_WORKLOAD 70 // Percentual de trabalho para a GPU
 int main(int argc, char *argv[]) {
-int N;
+    int numprocs, rank, salto;
+    long int num_primes_gpu = 0, num_primes_cpu = 0, num_primes=0;
+    long int  n, i, gpu_end;
+    double start_time, end_time;
+
     if (argc < 2) {
-        printf("Valor inválido! Entre com o valor do maior inteiro\n");
-       	return 0;
+        n = 100000000; // Valor padrão se nenhum argumento for fornecido
     } else {
-        N = strtol(argv[1], (char **) NULL, 10);
+        n = strtol(argv[1], (char **) NULL, 10);
     }
 
-    int total_primes_cpu = 0;
-    int total_primes_gpu = 0;
-
-    // Dividir o trabalho entre CPU e GPU
-    int split_point = (int)(N * .3); // 30% para CPU, 70% para GPU
-    if (split_point % 2 != 0) 
-        split_point--; // Se for ímpar, subtrai 1 para tornar par
-    double inicio = omp_get_wtime();
-    // Criar duas tarefas assíncronas
+    gpu_end = n  * GPU_WORKLOAD / 100;
+    if (gpu_end % 2 != 0) {
+        gpu_end--; // Se for ímpar, subtrai 1 para tornar par
+    }
+    salto = numprocs*2;
+    start_time = omp_get_wtime();
     #pragma omp parallel
     {
         #pragma omp master
         {
-            // Tarefa 1: Processamento na CPU
-            #pragma omp task shared(total_primes_cpu, split_point)
-            #pragma omp parallel for reduction(+:total_primes_cpu) num_threads(16)
-            for (int i = 3; i <= split_point; i +=2) {
-                 if (is_prime(i)) 
-                    total_primes_cpu++;
+        // Tarefa 1: Processamento na GPU
+        #pragma omp task shared(num_primes_gpu, rank, n)
+        {
+        #pragma omp target data map(tofrom:num_primes_gpu) device(1)
+        #pragma omp target teams distribute parallel for reduction(+:num_primes_gpu)
+            for (long int i = 3+(rank*2); i <= gpu_end; i += salto) {
+                if (is_prime(i)) num_primes_gpu++;
             }
-
-            // Tarefa 2: Processamento na GPU
-            #pragma omp task shared(total_primes_gpu, split_point)
-            #pragma omp target teams distribute parallel for reduction(+:total_primes_gpu) map(tofrom: total_primes_gpu)
-            for (int i = split_point + 1; i <= N; i+=2) {
-                if (is_prime(i)) 
-                    total_primes_gpu++;
+        }
+        // Tarefa 2: Processamento na CPU
+        #pragma omp task shared(num_primes_cpu, rank, n)
+        #pragma omp parallel for reduction(+:num_primes_cpu) num_threads(4)
+            for (long int j = gpu_end+(rank*2)+1; j <= n; j += salto) {
+                if (is_prime(j)) num_primes_cpu++;
             }
-            // Aguardar a conclusão das duas tarefas
-            #pragma omp taskwait
         }
     }
-
-    // Soma os resultados da CPU e GPU
-    int total_primes = total_primes_cpu + total_primes_gpu;
-    total_primes++; // Adiciona o número 2, que não foi verificado
-    double tempo = omp_get_wtime() - inicio;
-    printf("Total de números primos entre 1 e %d: %d. Calculado em %f segundos.\n", N, total_primes, tempo);
-    printf("Primos encontrados na CPU: %d\n", total_primes_cpu);
-    printf("Primos encontrados na GPU: %d\n", total_primes_gpu);
-    printf("Ponto de Divisão: %d\n", split_point);
+    num_primes = num_primes_gpu + num_primes_cpu+1; // Adiciona 2, que é o único número par primo
+    end_time = omp_get_wtime();
+    printf("Total de primos até %ld: %ld\n", n, num_primes);
+    printf("Calculado em %lf segundos\n", end_time - start_time);
+    }
     return 0;
 }
